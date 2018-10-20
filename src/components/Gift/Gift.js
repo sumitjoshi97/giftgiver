55237c5428bdf0ea3dfe25e2a22c6d9ee7084e48
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 16:37:42 +0530

    Gift component added

---
+++ src/components/Gift/Gift.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Button
+} from 'react-bootstrap'
+
+class Gift extends Component {
+  state = {
+    person: '',
+    present: ''
+  }
+
+  onInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Gift
+        <Form>
+          <FormGroup>
+            <ControlLabel>Person</ControlLabel>
+            <FormControl
+              name="person"
+              className="input-person"
+              onChange={e => this.setState({ person: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Present</ControlLabel>
+            <FormControl
+              name="present"
+              className="input-present"
+              onChange={e => this.setState({ present: e.target.value })}
+            />
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Gift
