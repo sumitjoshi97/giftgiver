cfd62a91d81646a3b8151260281d3505941152a7
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 14:52:55 +0530

    tests for root component

---
+++ src/components/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+const app = shallow(<App />)
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot()
+})
+
+it('initializes `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([])
+})
+
+it('adds a new gift to `state` when clicking `add gift` button', () => {
+  app.find('.btn-add').simulate('click')
+  expect(app.state().gifts).toEqual([{ id: 1 }])
+})
